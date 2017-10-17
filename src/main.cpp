c7496dec34540be054922d6db50d5f80bdc6245b
Benjamin BOITRELLE <benjamin.boitrelle@gmail.com>
2017-10-17 11:55:08 +0200

    Modification of main file to replace magic numbers with symbolic constant

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 //  Copyright © 2017 Benjamin BOITRELLE. All rights reserved.
 //
 
-
 #include <stdio.h>
 
 #include <iostream>
@@ -35,18 +34,20 @@ void splitString(const std::string& inputString, char delimiterChar, std::vector
 
 static int COLUMN = 128;
 static int THRESHOLD_POSITION = 0;
+static int ROW_BEGIN = 0;
+static int COLUMN_BEGIN = -1;
 
 int main(int argc, const char * argv[]) {
 
   std::string myLine;
   std::vector<std::vector<double>> tmpLine;
   std::vector<std::map<double, double>> pixel;
-  std::fstream myfile("scans.txt");
-  std::vector<double> threshold;
-  std::vector<double> response;
+  std::string outputDirectory = "output/";
+  std::string inputFile = "scans.txt";
+  std::fstream myInputFile(inputFile); 
 
   // Opening text file to scan it and read it line by line
-  while (std::getline(myfile, myLine)){
+  while (std::getline(myInputFile, myLine)){
     std::vector<std::string> result;
     splitString(myLine, ' ', result);
     std::vector<double> resultDoubleFormat(result.size());
@@ -57,22 +58,22 @@ int main(int argc, const char * argv[]) {
     tmpLine.push_back(resultDoubleFormat);
   }
 
-  int row(0), column(-1);
+  // Creating multiple file according to the number of columns needed
+  int row(ROW_BEGIN), column(COLUMN_BEGIN);
   for (auto iteratorPixel = 1; iteratorPixel < tmpLine.at(0).size(); iteratorPixel++){
     if ((iteratorPixel%COLUMN) != 0){
       column++;
     }
     else{
-      column = 0;
+      column = COLUMN_BEGIN;
       row++;
     }
-    std::ofstream outputFile("output/pixel_scan_row" + std::to_string(row) + "_col" + std::to_string(column) + ".txt", std::ios::out);
+    std::ofstream outputFile(outputDirectory + "pixel_scan_row" + std::to_string(row) + "_col" + std::to_string(column) + ".txt", std::ios::out);
     for (auto iteratorThreshold = 0; iteratorThreshold < tmpLine.size(); iteratorThreshold++){
       outputFile << tmpLine[iteratorThreshold][THRESHOLD_POSITION] << " " << tmpLine[iteratorThreshold][iteratorPixel] << std::endl;
     }
   }
-  myfile.close();
+  myInputFile.close();
 
   return 0;
 }
-
